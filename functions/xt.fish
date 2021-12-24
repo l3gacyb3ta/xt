1a96e967500134668d182dd739803c3e4cc01110
Arcade Wise <l3gacy.b3ta@gmail.com>
2021-12-24 16:11:57 -0500

    Add a description

--- functions/xt.fish
+++ functions/xt.fish
@@ -1,7 +1,7 @@
-function xtmd
+function xtmd -d "Read a MarkDown file in Zathura"
     cat $argv | pandoc -f markdown -t pdf | zathura -
 end
 
-function xtman
+function xtman -d "Read a man file in Zathura"
     man -t $argv | ps2pdf - - | zathura -
 end
