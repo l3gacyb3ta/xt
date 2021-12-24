6dcf1bcc7ad0c76038862a9389a451a5a263477c
Arcade Wise <l3gacy.b3ta@gmail.com>
2021-12-24 16:46:03 -0500

    Add error messages

--- functions/xt.fish
+++ functions/xt.fish
@@ -1,7 +1,15 @@
 function xtmd -d "Read a MarkDown file in Zathura"
-    cat $argv | pandoc -f markdown -t pdf | zathura -
+    if [ (echo $argv) != "" ]
+        cat $argv | pandoc -f markdown -t pdf | zathura -
+    else
+        echo "Please provide a file :3"
+    end
 end
 
 function xtman -d "Read a man file in Zathura"
-    man -t $argv | ps2pdf - - | zathura -
+    if [ (echo $argv) != "" ]
+        man -t $argv | ps2pdf - - | zathura -
+    else
+        echo "Please provide a file :3"
+    end
 end
